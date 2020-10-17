5d5d5e60f19c0d8c3c800fafbe18fbabf3340788
ray <68985149+Raymondvonz@users.noreply.github.com>
2020-10-17 23:50:47 +1100

    Add files via upload

---
+++ W8/count_down/count_down.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER); 
+  frameRate(2);
+}
+ 
+function draw() {
+  background(10);
+  fill(255); // 
+  textSize(100); // 
+  countdown(10, 'Hello CodeWords!');
+}
+ 
+function countdown(number, message){
+  var currentOutput;
+  if(frameCount <= number + 1 ){
+    currentOutput = number + 1 - frameCount % (number+2);
+  }else{
+    currentOutput = message;
+  }
+ 
+  text(currentOutput, width/2, height/2);
+}
